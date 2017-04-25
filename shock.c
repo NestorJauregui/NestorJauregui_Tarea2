0e26ee527ef430729d9876e12ac0286023838c64
Juan Camilo Gallego Gallon <jc.gallego10@uniandes.edu.co>
2017-04-25 18:56:03 -0500

    avance

--- shock.c
+++ shock.c
@@ -1,7 +1,64 @@
-#include <stdio.h>
+ 56#include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "shock.h"
 
 
+int main(){
 
+double dx = L/n_dis;
+
+double *rho = malloc((n_dis)*sizeof(double));
+double *P = malloc((n_dis)*sizeof(double));
+double *u = malloc((n_dis)*sizeof(double));
+
+double *U = malloc((n_dis*3.0)*sizeof(double));
+double *UB = malloc((n_dis*3.0)*sizeof(double));
+double *UA = malloc((n_dis*3.0)*sizeof(double));
+
+double *F = malloc((n_dis*3.0)*sizeof(double));
+double *FB = malloc((n_dis*3.0)*sizeof(double));
+double *FA = malloc((n_dis*3.0)*sizeof(double));
+
+
+
+return 0;
+}
+
+
+
+void init_array( double *U, double *UB,double *UA){
+  int i;
+  int j;
+  
+  for(i=0; i<n_dis; i++){
+    if(i<=(n_dis-1)/2){
+      U[matrix(0,i)] = rho1;
+      U[matrix(1,i)] = rho1 * U1;
+      U[matrix(2,i)] = (P1/(GAMMA+1))+rho1*U1*U1/2;
+      UA[matrix(0,i)] = rho1;
+      UA[matrix(1,i)] = rho1 * U1;
+      UA[matrix(2,i)] = (P1/(GAMMA+1))+rho1*U1*U1/2;
+      UB[matrix(0,i)] = rho1;
+      UB[matrix(1,i)] = rho1 * U1;
+      UB[matrix(2,i)] = (P1/(GAMMA+1))+rho1*U1*U1/2;
+     }
+  }
+}
+
+void calc_F( double *U, double *F){
+
+  for(i=0; i<n_dis ; i++){
+    
+    F[matrix(0,i)] = U[matrix(1,i)];
+    F[matrix(2,i)] = pow(U[matrix(1,i)],2)/U[matrix(0,i)] + (GAMMA-1) * (U[matrix(2,i)] -0.5* pow(U[matrix(1,i)],2)/U[matrix(0,i)]);
+    F[matrix(3,i)] = (U[matrix(1,i)] / U[matrix(0,i)]) *( U[matrix(2,i)] + (GAMMA-1) * (U[matrix(2,i)] - 0.5 * pow(U[matrix(1,i)],2)/U[matrix(0,i)]) ) ;
+    
+  }
+
+}
+
+
+int matrix(int fila, int columna){
+  return (n_dis*fila)+columna;
+}
--- shock.h
+++ shock.h
@@ -1,11 +1,15 @@
-#define GAMMA  500
+#define GAMMA  1.4
 
-#define rho1 = 1.0
-#define P1 = 1.0
-#define U1 = 0.0
+#define rho1 1.0
+#define P1 1.0
+#define U1 0.0
 
-#define P5 = 0.1
-#define rho5 = 0.1
-#define U5 = 0.0
-#define x0 = 0.5
-#define L = 1.0 
+#define P5 0.1
+#define rho5 0.125
+#define U5 0.0
+#define x0 0.5
+#define L 1.0 
+
+#define n_dis 501
+
+
